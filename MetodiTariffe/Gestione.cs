18ae6e63439673265b4305756ffe996fd2ce0245
Lucia Rossi <nicoconvito04@gmail.com>
2021-03-11 11:51:05 +0100

    Aggiungere i file di progetto.

---
+++ MetodiTariffe/Gestione.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetodiTariffe
+{
+    public class Gestione
+    {
+        public static double Tariffa1(int ore, double costotariffa)
+        {
+
+        }
+    }
+}
---
+++ NicoConvitoSviluppo/TestVerifica.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NicoConvitoSviluppo
+{
+    [TestClass]
+    public class TestVerifica
+    {
+        [TestMethod]
+        public void Tariffa1()
+        {
+
+        }
+    }
+}
