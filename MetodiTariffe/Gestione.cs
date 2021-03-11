4ce3291c7ebab0008b8bbde6e862864d0c0e0acc
Lucia Rossi <nicoconvito04@gmail.com>
2021-03-11 12:23:53 +0100

    Primi 3 metodi e test

--- MetodiTariffe/Gestione.cs
+++ MetodiTariffe/Gestione.cs
@@ -4,9 +4,32 @@ namespace MetodiTariffe
 {
     public class Gestione
     {
-        public static double Tariffa1(int ore, double costotariffa)
-        {
 
+
+        public static double Tariffa1(int ore, double costoTariffa)
+        {
+            double costoTot = 15;
+            return costoTot;
+
+        }
+        public static double Tariffa2(int ore, double costoTariffa)
+        {
+            double costoTot = ore * costoTariffa;
+            return costoTot;
+        }
+
+        public static double Tariffa3(int ore, double costoTariffa)
+        {
+            double costoTot = 0;
+            if (ore == 1)
+            {
+                costoTot = 0;
+            }
+            else
+            {
+                costoTot = (ore-1) * costoTariffa;
+            }
+            return costoTot;
         }
     }
 }
--- NicoConvitoSviluppo/TestVerifica.cs
+++ NicoConvitoSviluppo/TestVerifica.cs
@@ -1,14 +1,39 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MetodiTariffe;
 
 namespace NicoConvitoSviluppo
 {
     [TestClass]
     public class TestVerifica
     {
+
         [TestMethod]
         public void Tariffa1()
         {
+            int ore = 3;
+            double tariffa = 15;
+            double rispsosta_aspettata = 15;
+            double risposta_effettiva = Gestione.Tariffa1(ore, tariffa);
+            Assert.AreEqual(rispsosta_aspettata, risposta_effettiva);
+        }
+        [TestMethod]
+        public void Tariffa2()
+        {
+            int ore = 3;
+            double tariffa = 1;
+            double rispsosta_aspettata = 3;
+            double risposta_effettiva = Gestione.Tariffa2(ore, tariffa);
+            Assert.AreEqual(rispsosta_aspettata, risposta_effettiva);
+        }
 
+        [TestMethod]
+        public void Tariffa3()
+        {
+            int ore = 2;
+            double tariffa = 1.20;
+            double rispsosta_aspettata = 1.20;
+            double risposta_effettiva = Gestione.Tariffa3(ore, tariffa);
+            Assert.AreEqual(rispsosta_aspettata, risposta_effettiva);
         }
     }
 }
